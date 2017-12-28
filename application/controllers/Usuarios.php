cb008404334c09a7937172283fbd4ebec1e832c4
antonio <antonio.velezestevez@alum.uca.es>
2017-12-28 21:17:03 +0100

    Añadida plantilla principal de la página.

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -4,7 +4,9 @@ class Usuarios extends CI_Controller
 {
 	function index()
 	{
-		$this->load->view("home_page");
-	}
+		$this->load->view("layout/header", array("title" => "Mi pagina"));
+        $this->load->view("layout/navbar");
+        $this->load->view("layout/footer");
+    }
 }
 ?>
