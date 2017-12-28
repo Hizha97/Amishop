f88684bfc796016e1940cb3a843f2863bceb51f6
antonio <antonio.velezestevez@alum.uca.es>
2017-12-28 21:19:26 +0100

    Añadida plantilla principal de la página.

---
+++ application/views/layout/header.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <title><?php echo $title ?></title>
+
+    <!-- Bootstrap core CSS -->
+
+</head>
+
+<body>
+
