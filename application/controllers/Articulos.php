6cc113b147a173c2dde8376e500d87b3b84cec5e
hizha97 <hizha97@gmail.com>
2017-12-29 01:30:27 +0100

    Empezados controlador, vista y modelo de articulos

--- application/controllers/Articulos.php
+++ application/controllers/Articulos.php
@@ -11,12 +11,12 @@ class Articulos extends CI_Controller
     public function index()
     {
         $this->load->model("articulos_model");
-
         $data['articulos'] = $this->articulos_model->getArticulos()->result();
 
+        $this->load->view("layout/header", array("title" => "Amishop"));
+        $this->load->view("layout/navbar");
         $this->load->view("articulos_view", $data);
-        #$this->load->view("templates/header");
-
+        $this->load->view("layout/footer");
 
     }
 }
