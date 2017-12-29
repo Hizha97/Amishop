2b877ee650360267fa4812b35d15bb2af8e0a3cc
Hizha97 <hizha97@gmail.com>
2017-12-29 20:47:22 +0100

    Logo Amishop

--- application/views/newsletter_view.php
+++ application/views/newsletter_view.php
@@ -1,3 +1,26 @@
 <div class="container">
-
+    <br>
+    <div class="col-12 col-sm-12">
+        <div class="card text-white bg-success mb-3" style="max-width: 100rem;">
+            <div class="card-header">¿Desea subscribirse a nuestra newsletter?</div>
+            <div class="card-body">
+                <h4 class="card-title">¡Genial!</h4>
+                <p class="card-text">¡Estar subscrito significa enterarse antes que nadie de los nuevos productos y ofertas!</p>
+            </div>
+    </div>
+        <br>
+        <form action="newsletter/">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tu precioso email aquí" required>
+            <small id="emailHelp" class="form-text text-muted">Tranquilo, nadie dará tu información a terceros</small>
+        </div>
+        <div class="form-group">
+            <label class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" required>
+                <span class="custom-control-indicator"></span>
+                <span class="custom-control-description">Acepto recibir información por correo</span>
+            </label>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
 </div>
