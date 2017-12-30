d3776cf38ee9a4cfaf66f6260bc9323ef321b261
Hizha97 <hizha97@gmail.com>
2017-12-30 02:18:32 +0100

    Vista individual de cada articulo

--- application/views/articulo_view.php
+++ application/views/articulo_view.php
@@ -1,21 +1,29 @@
 <div class="container">
+    <?php
+    $articulo = $articulos[0]; ?>
+    <br>
     <div class="row">
-        <?php
-        foreach ($articulos as $articulo)
-        {
-            echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12">';
-            echo '<div class="card border-secondary mb-3" style="max-width: 20rem;">';
-            echo sprintf("<div class='card-header'> <a href='articulos/articulo/%s'> %s</a> </div>", $articulo->nombre, $articulo->nombre );
-            echo '<img style="height: 200px; width: 100%; display: block;" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';
-            echo '<div class="card-body text-secondary">';
-            echo "<h4 class='card-title'> $articulo->precio € </h4>";
-            echo "<p class='card-text'> $articulo->descripcion </p>";
-            echo '</div>';
-            //echo '<img style="height: 200px; width: 100%; display: block;" src= alt="Card image">';
-            echo '</div>';
-            echo "</div>";
-        }
-        ?>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <?php echo '<img class="img-fluid" alt="Responsive image" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';?>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="media">
+                <div class="media-body">
+                    <h2 class="mt-2"> <?php echo $articulo->nombre; ?> </h2>
+                    <?php echo $articulo->descripcion; ?>
+
+                    <div class="media mt-3">
+                        <a class="pr-3" href="#">
+                        </a>
+                        <div class="media-body">
+                            <h5 class="mt-0"> <strong><?php echo $articulo->precio . "€"; ?></strong></h5>
+                            <button type="button" class="btn btn-danger disabled">¡Al carrito!</button>
+                            <p class="text-danger">¡Oh no! No puedes añadir al carrito nada sin registarte antes.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
--- application/views/articulos_view.php
+++ application/views/articulos_view.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <div class="jumbotron">
+    <div class="jumbotron d-none d-sm-none d-md-block">
         <h1 class="display-3">¡Bienvenidos a Amishop!</h1>
         <p class="lead">Amigurumis hechos a mano y llenos de cariño a los que puedes adoptar y darles un hogar. ¿Por que no te animas y miras algunos de mis productos?</p>
         <hr class="my-4">
@@ -16,14 +16,13 @@
     foreach ($articulos as $articulo)
     {
         echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12">';
-            echo '<div class="card border-secondary mb-3" style="max-width: 20rem;">';
+            echo '<div class="card border-secondary mb-3">';
         echo sprintf("<div class='card-header'> <a href='articulos/articulo/%s'> %s</a> </div>", $articulo->nombre, $articulo->nombre );
-        echo '<img style="height: 200px; width: 100%; display: block;" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';
+        echo '<img class="img-thumbnail" display: block;" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';
         echo '<div class="card-body text-secondary">';
         echo "<h4 class='card-title'> $articulo->precio € </h4>";
         echo "<p class='card-text'> $articulo->descripcion </p>";
         echo '</div>';
-        //echo '<img style="height: 200px; width: 100%; display: block;" src= alt="Card image">';
             echo '</div>';
         echo "</div>";
     }
