7ac05add2989757eb05cca407d6212dfd0909a0c
antonio <antonio.velezestevez@alum.uca.es>
2018-01-03 18:58:32 +0100

    Login casi listo

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -23,14 +23,13 @@ class Usuarios extends CI_Controller
 
         if ($this->form_validation->run() == FALSE)
             $this->load->view("nuevoUsuario");
-        else
-        {
+        else {
             $this->load->model('usuarios_model');
-            if($this->usuarios_model->nuevoUsuario())
+            if ($this->usuarios_model->nuevoUsuario())
                 $this->load->view('registro_usuarios_exito');
             else {
                 $err = $this->db->error();
-                if(array_key_exists($err['code'], $errorMsgs))
+                if (array_key_exists($err['code'], $errorMsgs))
                     $data['msg'] = $errorMsgs[$err['code']];
                 else
                     $data['msg'] = "Error desconocido contacte con el administrador.";
@@ -45,7 +44,30 @@ class Usuarios extends CI_Controller
 
     function login()
     {
+        $this->load->helper("form");
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('nombreUsuario', 'Nombre de usuario', 'required');
+        $this->form_validation->set_rules('contrasena', 'Contraseña', 'required');
 
+        $this->load->view("layout/header", array("title" => "Inicio de sesión"));
+        $this->load->view("layout/navbar");
+
+        if ($this->form_validation->run() == FALSE)
+            $this->load->view("login", array('error_login' => false));
+        else {
+            $this->load->model('usuarios_model');
+            if($this->usuarios_model->iniciarSesion())
+            {
+                $this->session->set_userdata('isLoggedIn', TRUE);
+                $this->session->set_userdata('name', $this->usuarios_model->getNombre($this->input->post('nombreUsuario')));
+                $this->load->view("login_exito");
+            }
+            else
+               $this->load->view("login", array('error_login' => true, 'msg' => "El usuario, la contraseña o ambos son incorrectos."));
+
+        }
+
+        $this->load->view("layout/footer");
 
     }
 }
--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -10,8 +10,6 @@ class Usuarios_model extends CI_Model
 {
     public function nuevoUsuario()
     {
-        $this->load->helper('security');
-
         $data = array(
             "nombre" => $this->input->post('nombre'),
             "apellidos" => $this->input->post('apellidos'),
@@ -24,5 +22,37 @@ class Usuarios_model extends CI_Model
         $data['contrasena'] = password_hash($data['contrasena'], PASSWORD_BCRYPT, array("cost" => 12));
 
         return $this->db->insert('usuarios', $data);
+    }
+
+    public function iniciarSesion(){
+        $data = array(
+            "nombreUsuario" => $this->input->post('nombreUsuario'),
+            "contrasena" => $this->input->post('contrasena'),
+        );
+
+
+        $this->db->select('contrasena')
+                 ->from('usuarios')
+                 ->where('nombreUsuario =', $data['nombreUsuario'])
+                 ->or_where('email =', $data['nombreUsuario']);
+
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1)
+            return password_verify($data['contrasena'], $query->result()[0]->contrasena);
+        else return false;
+    }
+
+    public function getNombre($username){
+
+        $this->db->select('nombre')
+            ->from('usuarios')
+            ->where('nombreUsuario =', $username);
+
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1)
+            return $query->result()[0]->nombre;
+        else return false;
     }
 }
