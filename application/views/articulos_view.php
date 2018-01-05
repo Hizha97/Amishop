7d4d386c67d3c3203f934161fcadce82ccc7536b
antonio <antonio.velezestevez@alum.uca.es>
2018-01-05 16:02:40 +0100

    Arreglando error en el commit anterior

--- application/views/articulos_view.php
+++ application/views/articulos_view.php
@@ -1,4 +1,5 @@
-<div class="jumbotron jumbotron-fluid d-d-sm-none d-md-block">
+<div class="container-fluid">
+    <div class="jumbotron d-none d-sm-none d-md-block">
         <h1 class="display-3">¡Bienvenidos a Amishop!</h1>
         <p class="lead">Amigurumis hechos a mano y llenos de cariño a los que puedes adoptar y darles un hogar. ¿Por que no te animas y miras algunos de mis productos?</p>
         <hr class="my-4">
@@ -6,26 +7,25 @@
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="newsletter" role="button">¡Quiero subscribirme!</a>
         </p>
+    </div>
 </div>
 <br>
 <div class="container">
     <div class="row">
-    <?php
-    foreach ($articulos as $articulo)
-    {
-        echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12">';
+        <?php
+        foreach ($articulos as $articulo)
+        {
+            echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12">';
             echo '<div class="card border-secondary mb-3">';
-        echo "<div class='card-header'> $articulo->nombre </div>";
-        echo '<img style="height: 200px; width: 100%; display: block;" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';
-        echo '<div class="card-body text-secondary">';
-        echo "<h4 class='card-title'> $articulo->precio € </h4>";
-        echo "<p class='card-text  text-truncate'> $articulo->descripcion </p>";
-        echo '</div>';
-        //echo '<img style="height: 200px; width: 100%; display: block;" src= alt="Card image">';
+            echo sprintf("<div class='card-header'> <a href='articulos/articulo/%s'> %s</a> </div>", $articulo->nombre, $articulo->nombre );
+            echo '<img class="img-thumbnail" display: block;" src="data:image/jpeg;base64,'.base64_encode($articulo->imagen) .'" />';
+            echo '<div class="card-body text-secondary">';
+            echo "<h4 class='card-title'> $articulo->precio € </h4>";
+            echo "<p class='card-text'> $articulo->descripcion </p>";
             echo '</div>';
-        echo "</div>";
-    }
-    ?>
+            echo '</div>';
+            echo "</div>";
+        }
+        ?>
     </div>
 </div>
-
